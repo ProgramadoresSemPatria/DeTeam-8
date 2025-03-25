88689b350a27f7dec960e98dab32ed0007b2970b
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-03-25 11:05:16 -0300

    feat: import resources component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,14 @@
 
 import Hero from "./components/general/Hero";
-import CallToAction from "./components/general/CallToAction";
+import { Button } from "./components/ui/button";
+import Resources from "./components/general/Resources";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <Hero />
-
+      <Resources />
       {/* CTA Section */}
-      <CallToAction />
     </div>
   );
 }
