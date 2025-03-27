28b4385e9c1d0f09c32b83911c7840ee1587046f
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-03-27 11:58:21 -0300

    feat: create header component with nav

---
+++ frontend/components/layout/Header.tsx
@@ -0,0 +1,44 @@
+import { Link } from 'next-view-transitions';
+
+export default function Navbar() {
+  return (
+    <header className="max-w-7xl mx-auto py-5 sticky top-0 z-50 bg-white">
+      <nav className='flex items-center justify-between'>
+        <div className="flex items-center gap-6">
+          <Link href="/">
+            <p className="text-3xl font-semibold">
+              <span className="text-blue-500">EventoFacil</span>
+            </p>
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-6">
+          <Link
+            className="text-md font-medium hover:text-blue-500 transition-colors"
+            href="/sign-in"
+          >
+            Login
+          </Link>
+          <Link
+            className="text-md font-medium hover:text-blue-500 transition-colors"
+            href="/sign-up"
+          >
+            Sign up
+          </Link>
+        </div>
+      </nav>
+
+      {/* {user ? (
+        <div className="flex items-center gap-4">
+          <p>{user.name}</p>
+          <button>Logout</button>
+        </div>
+      ) : (
+        <div className="flex items-center gap-4">
+          <button>Login</button>
+          <button>Sing up</button>
+        </div>
+      )} */}
+    </header>
+  );
+}
