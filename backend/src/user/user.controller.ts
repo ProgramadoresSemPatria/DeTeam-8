f71258b20eda77b0becce976343a29d4995ae884
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-03-28 17:44:56 -0300

    feat: create user/me route with jwt guard

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,15 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+
+export interface RequestUser extends Request {
+  user: {
+    id: string;
+    email: string;
+  };
+}
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
+  @Post('register')
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  async getMe(@Req() req: RequestUser) {
+    console.log('🔹 Recebendo requisição GET /user/me');
+    return this.userService.findOne(req.user.id); 
   }
 
   @Get()
@@ -17,18 +32,26 @@ export class UserController {
     return this.userService.findAll();
   }
 
+  // TODO: implementar autenticação
+  //@UseGuards(AuthGuard('jwt'))
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+    console.log(`Recebendo requisição para /user/${id}`);
+
+    return this.userService.findOne(id);
   }
 
+  // TODO: implementar autenticação
+  //@UseGuards(AuthGuard('jwt'))
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+    return this.userService.update(id, updateUserDto);
   }
 
+  // TODO: implementar autenticação
+  //@UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+    return this.userService.remove(id);
   }
 }
