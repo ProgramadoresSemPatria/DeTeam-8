3adee36364df36fc044c91c5f5eef6ca12a50a54
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-03-28 23:22:01 -0300

    feat: create component to protect route

---
+++ frontend/components/layout/ProtectedRoute.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useEffect } from 'react';
+import useAuthContext from '@/hooks/auth/useAuthContext';
+import { useRouter } from 'next/navigation';
+
+export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
+    const { isLogged } = useAuthContext();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!isLogged) {
+            router.push('/sign-in'); 
+        }
+    }, [isLogged, router]);
+
+    if (!isLogged) {
+        return null; 
+    }
+
+    return <>{children}</>; 
+}
