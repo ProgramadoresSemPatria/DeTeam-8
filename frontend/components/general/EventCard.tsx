e7b1a06d2e97194fc6e8bd80ffb18f6b0f4bb271
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-03-30 10:40:07 -0300

    feat: add event img url

--- frontend/components/general/EventCard.tsx
+++ frontend/components/general/EventCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { formattedDate } from "@/util/functions/formattedDate";
-import { Event } from "@/util/types/event";
+import { EventTypes } from "@/util/types/event";
 import { motion } from "framer-motion";
 import { CalendarDays, MapPin, Users } from "lucide-react";
 import Image from "next/image";
@@ -9,17 +9,31 @@ import Link from "next/link";
 import { Badge } from "../ui/badge";
 import { EventCardSkeleton } from "./EventCardSkeleton";
 import useAuthContext from "@/hooks/auth/useAuthContext";
+import { useEffect, useState } from "react";
+import { defaultImgUrl } from "@/util/constants";
+import { useGetEventImgUrl } from "@/hooks/useGetEventImgUrl";
 
 
 interface EventCardProps {
-  event: Event;
+  event: EventTypes;
   isLoading?: boolean;
 }
 
 export default function EventCard({ event, isLoading = false }: EventCardProps) {
 
   const { isLogged } = useAuthContext();
+  const [imageUrl, setImageUrl] = useState<string>(defaultImgUrl);
 
+  const { data: pexelsImageUrl } = useGetEventImgUrl(event?.title as string);
+
+  useEffect(() => {
+    if(!event?.title) return
+    
+    if(pexelsImageUrl) {
+        setImageUrl(pexelsImageUrl);
+    }
+  }, [event?.title, pexelsImageUrl]);
+  
   if (isLoading) {
     return <EventCardSkeleton />;
   }
@@ -32,7 +46,7 @@ export default function EventCard({ event, isLoading = false }: EventCardProps) 
       className=" bg-white rounded-md border border-gray-200">
       <div className="flex-1 h-[12rem] relative">
         <Image
-          src={event.imageUrl || "/placeholder.png"}
+          src={imageUrl}
           width={400}
           height={400}
           alt="Imagem do evento"
@@ -43,12 +57,12 @@ export default function EventCard({ event, isLoading = false }: EventCardProps) 
           aria-label="Presencial ou Online"
           className={`absolute top-2 right-2 text-[.65rem]
                     ${
-                      event.type === "Presencial"
+                      event.type.toLowerCase() === "presencial"
                         ? "border-gray-300 bg-gray-100/90 black"
                         : "text-blue-800 bg-blue-100/90 border-blue-300"
                     }`}
         >
-          {event.type}
+          {event.type === "presencial" ? "Presencial" : "Online"}
         </Badge>
       </div>
       <div className="p-4 flex w-full h-fit flex-col justify-between text-black space-y-1">
@@ -64,8 +78,8 @@ export default function EventCard({ event, isLoading = false }: EventCardProps) 
           </div>
           <div className="flex items-center">
             <Users className="h-4 w-4 mr-2 text-muted-foreground" />
-            <span>
-              {event.registered} / {event.capacity} participantes
+            <span> {/* TODO: Adicionar numero de Guests */}
+              {/* {event.registered} / {event.capacity} */} participantes
             </span>
           </div>
         </div>
