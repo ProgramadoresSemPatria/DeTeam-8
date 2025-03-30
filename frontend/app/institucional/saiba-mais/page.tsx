b024d73827401bf4c5ca1e47dfa1fc1422a95cbb
luiznwz <luizrenan179@gmail.com>
2025-03-30 20:37:35 -0300

    feat(saiba-mais): add HowWorks component and improve UI

--- frontend/app/institucional/saiba-mais/page.tsx
+++ frontend/app/institucional/saiba-mais/page.tsx
@@ -3,18 +3,15 @@ import React from "react";
 import FAQSection from "@/components/general/FAQSection";
 import Pricing from "@/components/general/Pricing";
 import Testimonials from "@/components/general/Testimonials";
+import HowWorks from "@/components/general/HowWorks";
 
 const SaibaMais = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
-
-
+    <div className="min-h-screen">
+      <HowWorks />
       <Testimonials />
-
       <Pricing />
-
       <FAQSection />
-
     </div>
   );
 };
