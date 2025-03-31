af050f2c8b3d9f5e3f80d9bb8343167e6a5a8480
Patrick Simões <patricksimoes25@gmail.com>
2025-03-30 23:12:13 -0300

    feat: add node image docker

--- backend/src/database/orm-cli-config.ts
+++ backend/src/database/orm-cli-config.ts
@@ -3,7 +3,7 @@ import { DataSource, DataSourceOptions } from 'typeorm'
 
 export const dataSourceOptions: DataSourceOptions = {
     type: 'postgres',
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT),
     database: process.env.DB_NAME,
     username: process.env.DB_USER,
