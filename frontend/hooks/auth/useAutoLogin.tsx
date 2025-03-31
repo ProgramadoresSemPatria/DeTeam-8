6277b457041b2a9d0543db18abc3eefc336756d7
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-03-31 18:22:23 -0300

    refactor: corrects autologin redirectioning

--- frontend/hooks/auth/useAutoLogin.tsx
+++ frontend/hooks/auth/useAutoLogin.tsx
@@ -3,10 +3,11 @@ import { useEffect, useState } from "react";
 import { useValidateToken } from "./useValidateToken";
 import { projectConstants } from "@/util/constants";
 import useAuthContext from "./useAuthContext";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export const useAutoLogin = () => {
   const { setUser, setIsLogged } = useAuthContext();
+  const pathname = usePathname();
   const router = useRouter();
   const [token, setToken] = useState<string | null>(null);
 
@@ -22,12 +23,14 @@ export const useAutoLogin = () => {
     if (userData) {
       setUser(userData);
       setIsLogged(true);
-      router.push("/painel");
+      if(pathname === '/sign-in' || pathname === '/sign-up') {
+        router.push('/')
+      }
     }
 
     if (error) {
       localStorage.removeItem(projectConstants.token);
       setIsLogged(false);
     }
-  }, [userData, error, router, setIsLogged, setUser]);
+  }, [userData, error, router, setIsLogged, setUser, pathname]);
 };
