38f92c51118db5e7ad13d0c2d49ec2e05d5364a5
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-03-31 18:22:56 -0300

    feat: create use-get-guests-for-events fun

--- frontend/services/guestFunctions.ts
+++ frontend/services/guestFunctions.ts
@@ -1,22 +1,31 @@
 import axiosInstance from "./axiosInstance";
-import { useQuery } from "@tanstack/react-query";
+import { useQueries, useQuery } from "@tanstack/react-query";
 import { GuestOnEvent } from "@/util/types/guest";
 
 const getGuests = async (eventId: string) => {
     try {
         const { data } = await axiosInstance.get(`/guests/by-event/${eventId}`);
-        return data as GuestOnEvent;
+        return data as GuestOnEvent; 
     } catch (error) {
         console.error("Erro ao carregar eventos:", error);
-        throw error;
+        return 0; 
     }
 };
-export const useGetGuestsByEvent = (eventId: string) => {
-    const query = useQuery({
-        queryKey: ['guests-on-event'],
-        queryFn: () => getGuests(eventId),
-        enabled: !!eventId
-    })
 
-    return query
-}
+export const useGetGuestsByEvent = (eventId: string) => {
+    return useQuery({
+        queryKey: ['guests-on-event', eventId],
+        queryFn: () => getGuests(eventId),
+        enabled: !!eventId,
+    });
+};
+
+export const useGetGuestsForEvents = (events: { id: string }[]) => {
+    return useQueries({
+        queries: events.map((event) => ({
+            queryKey: ['guests-on-event', event.id],
+            queryFn: () => getGuests(event.id),
+            enabled: !!event.id,
+        })),
+    });
+};
