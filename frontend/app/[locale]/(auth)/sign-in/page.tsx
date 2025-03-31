43664c3ed9bf85a6dd226b8b59b8f628bb470f17
luiznwz <luizrenan179@gmail.com>
2025-03-30 23:57:34 -0300

    feat(auth): add new translations and integrate them in sign-in page

--- frontend/app/[locale]/(auth)/sign-in/page.tsx
+++ frontend/app/[locale]/(auth)/sign-in/page.tsx
@@ -2,17 +2,19 @@
 import { SignInForm } from "@/components/layout/AuthForm";
 import { AuthLayout } from "@/components/layout/AuthLayout";
 import { useAutoLogin } from "@/hooks/auth/useAutoLogin";
+import { useTranslations } from "next-intl";
 
 export default function SignIn() {
+  const t = useTranslation("Auth");
 
     useAutoLogin();
 
     return (
       <AuthLayout
-        title="Bem-vindo de volta"
-        subtitle="Entre com suas credenciais"
-        footerText="Não possui uma conta?"
-        footerLinkText="Crie uma"
+        title={t("title")}
+        subtitle={t("subtitle")}
+        footerText={t("footerText")}
+        footerLinkText={t("footer-link")}
         footerLinkHref="/sign-up"
       >
         <SignInForm />
