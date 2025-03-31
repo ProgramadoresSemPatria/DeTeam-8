55949666694b792bd3e5f46b6e1a0b34932a0010
luiznwz <luizrenan179@gmail.com>
2025-03-31 00:06:29 -0300

    feat(auth): add localized text for sign-up page

--- frontend/app/[locale]/(auth)/sign-up/page.tsx
+++ frontend/app/[locale]/(auth)/sign-up/page.tsx
@@ -1,14 +1,17 @@
 import { SignUpForm } from "@/components/layout/AuthForm";
 import { AuthLayout } from "@/components/layout/AuthLayout";
+import { useTranslations } from "next-intl";
 
 export default function SignUp() {
+  const t = useTranslations("Auth");
+
   return (
     <AuthLayout
-      title="Crie sua conta"
-      subtitle="Para organizar seus eventos"
-      footerText="Ja possui uma conta?"
-      footerLinkText="Entre agora"
-      footerLinkHref="/sign-in"
+    title={t("title-up")}
+    subtitle={t("subtitle-up")}
+    footerText={t("footer-text-up")}
+    footerLinkText={t("footer-link-up")}
+    footerLinkHref="/sign-in"
     >
       <SignUpForm />
     </AuthLayout>
