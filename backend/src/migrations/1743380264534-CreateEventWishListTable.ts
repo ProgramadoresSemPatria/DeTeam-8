54e7c3bc9b8253f8935a55db168ee6c9f9e3fdd5
Patrick Simões <patricksimoes25@gmail.com>
2025-03-30 21:19:54 -0300

    fix: adjustment timestamp migration

--- backend/src/migrations/1743380264534-CreateEventWishListTable.ts
+++ backend/src/migrations/1743380264534-CreateEventWishListTable.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateEventWishListTable1620000000005 implements MigrationInterface {
+export class CreateEventWishListTable1743380264534 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
