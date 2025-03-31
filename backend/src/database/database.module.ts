cfd5da49b5bb8a41e25a9a7ab625ead7766dbb36
Vandilson Brito <108373095+vandilsonbrito@users.noreply.github.com>
2025-03-31 13:42:21 -0300

    fix: docker prod config

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,11 +1,22 @@
-import { Module } from '@nestjs/common'
-import { TypeOrmModule } from '@nestjs/typeorm'
-import { ConfigService } from '@nestjs/config'
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useFactory: async (configService: ConfigService) => {
+        const databaseUrl = configService.get<string>('DATABASE_URL');
+        if (databaseUrl) {
+          return {
+            type: 'postgres',
+            url: databaseUrl,
+            entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+            logging: ['error', 'schema'],
+            synchronize: configService.get<string>('NODE_ENV') === 'DEVELOPMENT',
+            ssl: { rejectUnauthorized: false },
+          };
+        }
         return {
           type: 'postgres',
           host: configService.get<string>('DB_HOST') || 'localhost',
@@ -15,11 +26,11 @@ import { ConfigService } from '@nestjs/config'
           password: configService.get<string>('DB_PASSWORD'),
           entities: [__dirname + '/../**/*.entity{.ts,.js}'],
           logging: ['error', 'schema'],
-          synchronize: configService.get<string>('NODE_ENV') == 'DEVELOPMENT',
-        }
+          synchronize: configService.get<string>('NODE_ENV') === 'DEVELOPMENT',
+        };
       },
       inject: [ConfigService],
     }),
   ],
 })
-export class DatabaseModule { }
+export class DatabaseModule {}
--- backend/src/database/orm-cli-config.ts
+++ backend/src/database/orm-cli-config.ts
@@ -4,7 +4,7 @@ import { DataSource, DataSourceOptions } from 'typeorm'
 export const dataSourceOptions: DataSourceOptions = {
     type: 'postgres',
     host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT),
+    port: Number(process.env.DB_PORT) || 5432,
     database: process.env.DB_NAME,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
