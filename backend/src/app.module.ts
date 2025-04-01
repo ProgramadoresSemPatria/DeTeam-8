5cbbcb3d1ec9e7b559e6f7bf763b1acf0301d40d
Patrick Simões <patricksimoes25@gmail.com>
2025-03-31 21:47:10 -0300

    feat: add throttler configuration (3 req/sec, 20 req/10sec, 100 req/min)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { EventsModule } from './events/events.module';
 import { GuestsModule } from './guests/guests.module';
@@ -8,6 +8,8 @@ import { UserModule } from './user/user.module';
 import { CatalogModule } from './catalog/catalog.module';
 import { WishListModule } from './wish-list/wish-list.module';
 import { EventWishListModule } from './event-wish-list/event-wish-list.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -15,6 +17,23 @@ import { EventWishListModule } from './event-wish-list/event-wish-list.module';
       envFilePath: '.env',
       isGlobal: true
     }),
+    ThrottlerModule.forRoot([
+      {
+        name: 'short', // The configuration allows up to 3 requests per second.
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium', // The configuration allows up 20 requests every 10 seconds.
+        ttl: 10000,
+        limit: 20
+      },
+      {
+        name: 'long', // The configuration allows up 100 requests per minute.
+        ttl: 60000,
+        limit: 100
+      }
+    ]),
     DatabaseModule,
     EventsModule,
     GuestsModule,
@@ -23,6 +42,12 @@ import { EventWishListModule } from './event-wish-list/event-wish-list.module';
     CatalogModule,
     WishListModule,
     EventWishListModule
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 
